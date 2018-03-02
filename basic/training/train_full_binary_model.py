57d6b00f51a194c97c42a98c14fb7d9b716bd8a8
ALEC CHAPMAN <u0752374@ad.utah.edu>
2018-03-02 15:14:09 -0700

    created separate scripts for lexical vs. full feature sets

---
+++ basic/training/train_full_binary_model.py
@@ -0,0 +1,154 @@
+import sklearn
+import pickle
+import os, sys
+
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score, cross_val_predict, GridSearchCV
+
+from sklearn.utils import shuffle
+
+import train_utils
+
+DATADIR = os.path.join('..', '..', 'data')
+MODELDIR = os.path.join('..', '..', 'saved_models')
+assert os.path.exists(DATADIR)
+assert os.path.exists(MODELDIR)
+
+sys.path.append('..')
+sys.path.append('../../feature_extraction')
+
+label_mapping = {'none': 0,
+                'do':1,
+                'reason': 2,
+                'fr': 3,
+                'severity_type': 4,
+                'manner/route': 5,
+                'adverse': 6,
+                'du': 7
+                }
+
+def train_models(X, y):
+    """
+    Tries several classifications, saves cross-validated scores.
+    """
+    f = open('binary_model_scores.txt', 'w')
+    clfs = [LogisticRegression, DecisionTreeClassifier, MultinomialNB,
+    RandomForestClassifier, SVC]
+    for classifier in clfs:
+        clf = classifier()
+        print("Training {}".format(classifier))
+        pred = cross_val_predict(clf, X, y)
+        score = classification_report(y, pred)
+        print(score)
+        f.write('{}\n'.format(clf))
+        f.write(score)
+        f.write('\n\n')
+
+    f.close()
+
+def train_grid_search(X, y):
+    # Transform y
+    #y = [int(0) if label == 'none' else int(1)  for label in y_non_bin]
+
+    #X = transform_features(X)
+
+    #clf = LinearRegression()
+    #train_models(X, y)
+    clf = SVC()
+    clf_name = 'SVC'
+    param_grid = [
+            {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+            {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+            ]
+    learned_parameters = train_utils.grid_search(X, y, clf, param_grid)
+    with open('best_{}_params.pkl'.format(clf_name), 'wb') as f:
+        pickle.dump(parameters, f)
+    return learned_parameters
+
+
+def get_positive_preds(y_pred, pos='any'):
+    """
+    Gets the indices for any data points that were predicted to have a relation.
+    """
+    idxs = []
+    for i, y_ in enumerate(y_pred):
+        if y_ == pos:
+            idxs.append(i)
+    return idxs
+
+def train_eval_cross_val(X, y):
+    """
+    Trains and validates a model using cross-validation.
+    """
+    clf = RandomForestClassifier(max_depth = None,
+                            max_features = None,
+                            min_samples_leaf = 2,
+                            min_samples_split = 2,
+                            n_estimators = 10,
+                            n_jobs = 3)
+
+    pred = cross_val_predict(clf, X, y)
+    score = classification_report(y, pred)
+    print(score)
+    with open('rf_binary_model_cross_val_scores.txt', 'w') as f:
+        f.write(score)
+    return clf
+
+
+def train_model(X, y):
+    """
+    Trains a model using the entire training data.
+    Saves the trained model.
+    No evaluation.
+    """
+    clf = RandomForestClassifier(max_depth = None,
+                            max_features = None,
+                            min_samples_leaf = 2,
+                            min_samples_split = 2,
+                            n_estimators = 10,
+                            n_jobs = 3)
+    clf.fit(X, y)
+    outpath = os.path.join(MODELDIR, 'rf_binary_model.pkl')
+    with open(outpath, 'wb') as f:
+        pickle.dump(clf, f)
+    print("Saved at {}".format(outpath))
+    return clf
+
+
+def main():
+    inpath = os.path.join(DATADIR, data_file)
+    with open(inpath, 'rb') as f:
+        feat_dicts, relats, X, y, = pickle.load(f)
+
+    shuffle(feat_dicts, relats, X ,y)
+    #X = X[:10, :]
+    #y = y[:10]
+    print("X: {}".format(X.shape))
+    print("y: {}".format(len(y)))
+    print(set(y))
+    y = np.array(y)
+    print(y[:10])
+
+    train_eval_cross_val(X, y)
+
+    train_model(X, y)
+
+
+    #clf = SVC(C = learned_parameters['C'], 'kernel')
+
+    exit()
+
+
+
+
+
+if __name__ == '__main__':
+    data_file = 'binary_full_data.pkl'
+    main()
---
+++ basic/training/train_full_full_model.py
@@ -0,0 +1,119 @@
+import sklearn
+import pickle
+import os, sys
+
+import numpy as np
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.utils import shuffle
+
+import train_utils
+
+
+DATADIR = os.path.join('..', '..', 'data')
+MODELDIR = os.path.join('..', '..', 'saved_models')
+assert os.path.exists(DATADIR)
+assert os.path.exists(MODELDIR)
+sys.path.append('..')
+sys.path.append('../../feature_extraction')
+
+LABEL_MAPPING = {'none': 0,
+                'do':1,
+                'reason': 2,
+                'fr': 3,
+                'severity_type': 4,
+                'manner/route': 5,
+                'adverse': 6,
+                'du': 7
+                }
+
+def transform_features(X):
+    with open(os.path.join(MODELDIR, 'full_chi2.pkl'), 'rb') as f:
+        chi2 = pickle.load(f)
+    X = chi2.transform(X)
+    print("New X shape: {}".format(X.shape))
+    return X
+
+def filter_by_idx(idxs, *args):
+    arrs = args
+    z = list(zip(*arrs))
+    z = [z[i] for i in idxs]
+    return zip(*z)
+
+def train_eval_cross_val(X, y):
+    """
+    Trains and validates a model using cross-validation.
+    """
+    clf = RandomForestClassifier(max_depth = None,
+                            max_features = None,
+                            min_samples_leaf = 2,
+                            min_samples_split = 2,
+                            n_estimators = 10,
+                            n_jobs = 3)
+
+    pred = cross_val_predict(clf, X, y)
+    score = classification_report(y, pred)
+    print(score)
+    with open('rf_full_full_model_cross_val_scores.txt', 'w') as f:
+        f.write(score)
+    return clf
+
+
+def train_model(X, y):
+    """
+    Trains a model using the entire training data.
+    Saves the trained model.
+    No evaluation.
+    """
+    clf = RandomForestClassifier(max_depth = None,
+                            max_features = None,
+                            min_samples_leaf = 2,
+                            min_samples_split = 2,
+                            n_estimators = 10,
+                            n_jobs = 3)
+    clf.fit(X, y)
+    outpath = os.path.join(MODELDIR, 'rf_full_full_model.pkl')
+    with open(outpath, 'wb') as f:
+        pickle.dump(clf, f)
+    print("Saved full classifier at {}".format(outpath))
+    return clf
+
+
+def main():
+    inpath = os.path.join(DATADIR, data_file)
+    with open(inpath, 'rb') as f:
+        feat_dicts, relats, X, y, _, _ = pickle.load(f) #TODO: get rid of the _'s
+    shuffle(feat_dicts, relats, X ,y)
+    #X = X[:10000, :]
+    #y = y[:10000]
+    print("X: {}".format(X.shape))
+    print("y: {}".format(len(y)))
+    print(len(feat_dicts))
+    print(len(relats))
+    # Filter out any examples that the filtering classifier said had no relation
+    #with open('bin_yes_preds.pkl', 'rb') as f:
+    #    idxs = pickle.load(f)
+    #X = X[idxs]
+    #feat_dicts, relats, y = filter_by_idx(idxs, feat_dicts, relats, y)
+    print("X: {}".format(X.shape))
+    print("y: {}".format(len(y)))
+    print(len(feat_dicts))
+    print(len(relats))
+    y = np.array(y)
+    print(y[:10])
+
+    train_eval_cross_val(X, y)
+    train_model(X, y)
+    exit()
+
+
+
+
+
+if __name__ == '__main__':
+    data_file = 'full_full_data.pkl'
+    main()
